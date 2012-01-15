0608cd1c99f452e11e50ce49dd4aa29507a0e453
Eduardo Gulias <eduardomgulias@gmail.com>
2012-01-15 16:29:23 +0100

    Added two more Answer response types

---
+++ Model/Answers/AnswerResponseText.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Egulias\QuizBundle\Model\Answers;
+
+/**
+ * @author Eduardo Gulias Davis
+ * @package EguliasQuizBundle
+ * @subpackage Model
+ **/
+class AnswerResponseText extends AnswerResponse
+{
+
+}
---
+++ Model/Answers/AnswerResponseTextarea.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Egulias\QuizBundle\Model\Answers;
+
+/**
+ * @author Eduardo Gulias Davis
+ * @package EguliasQuizBundle
+ * @subpackage Model
+ **/
+class AnswerResponseTextarea extends AnswerResponse
+{
+
+}
