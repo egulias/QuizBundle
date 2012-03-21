6a2c8b4b1fa3bd4b42da124c7ce42181e4860623
Eduardo Gulias <eduardomgulias@gmail.com>
2012-03-22 00:14:05 +0100

    #1 - Fixing Travis

--- vendor/vendors.php
+++ vendor/vendors.php
@@ -12,13 +12,8 @@
 
 set_time_limit(0);
 
-if (isset($argv[1])) {
-    $_SERVER['SYMFONY_VERSION'] = $argv[1];
-}
-
 $vendorDir = __DIR__;
 $deps = array(
-    array('symfony', 'git://github.com/symfony/symfony.git', isset($_SERVER['SYMFONY_VERSION']) ? $_SERVER['SYMFONY_VERSION'] : 'origin/master'),
     array('doctrine-common', 'git://github.com/doctrine/common.git', 'origin/master'),
     array('doctrine-dbal', 'git://github.com/doctrine/dbal.git', 'origin/master'),
     array('doctrine', 'git://github.com/doctrine/doctrine2.git', 'origin/master'),
